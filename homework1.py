845e98ff6af55f1959fa63b9284693db45c09174
KuznecovIC <hlebovkamensik8@gmail.com>
2024-12-11 22:09:19 +0300

    дописал програмку которая говорит твою оценку

--- homework1.py
+++ homework1.py
@@ -2,4 +2,18 @@ letters = ['nikita', 'vladick', 'timocha']
 for letter in letters:
     print(letters.index(letter))
     print(letter)
-    
+
+
+
+
+
+gdz = input('Введите оценку:')
+if gdz == '5':
+       print('отлично')
+elif gdz <= '4':
+        print("нормис")
+elif gdz == '3':
+        print('каеф')
+elif gdz == '2':
+        print('плаки плаки')        
+          
